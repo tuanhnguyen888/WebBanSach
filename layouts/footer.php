d62f4070db5d66430dabcf389bbc6ae46e7980d9
TRẦN VĂN ĐIỆP <tranvandiep.it@gmail.com>
2021-10-26 15:34:08 +0700

    add home page

---
+++ layouts/footer.php
@@ -0,0 +1,39 @@
+<footer style="background-color: #81d742 !important;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h4>GIỚI THIỆU</h4>
+				<ul>
+					<li>LIÊN HỆ CÔNG TY CỔ PHẦN ZICZAC GROUP</li>
+					<li><i class="bi bi-mailbox2"></i> gokisoft.com@gmail.com</li>
+					<li><i class="bi bi-telephone-fill"></i> 123456789</li>
+					<li><i class="bi bi-map-fill"></i> Ha Noi, Viet Nam</li>
+					<li>Chúng tôi luôn tiên phong trong lĩnh vực xậy dựng website cho các doanh nghiệp và của hàng. Chúng tôi luôn nỗ lực để tạo ra sản phẩm có chất lượng tốt nhất cho khách hàng.</li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<h4>SẢN PHẨM MỚI NHẤT</h4>
+				<ul>
+					<li>LIÊN HỆ CÔNG TY CỔ PHẦN ZICZAC GROUP</li>
+					<li>Email: gokisoft.com@gmail.com</li>
+					<li>Phone: 123456789</li>
+					<li>Chúng tôi luôn tiên phong trong lĩnh vực xậy dựng website cho các doanh nghiệp và của hàng. Chúng tôi luôn nỗ lực để tạo ra sản phẩm có chất lượng tốt nhất cho khách hàng.</li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<h4>TIN TỨC MỚI NHẤT</h4>
+				<ul>
+					<li>LIÊN HỆ CÔNG TY CỔ PHẦN ZICZAC GROUP</li>
+					<li>Email: gokisoft.com@gmail.com</li>
+					<li>Phone: 123456789</li>
+					<li>Chúng tôi luôn tiên phong trong lĩnh vực xậy dựng website cho các doanh nghiệp và của hàng. Chúng tôi luôn nỗ lực để tạo ra sản phẩm có chất lượng tốt nhất cho khách hàng.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div style="background-color: #3f9609; width: 100%; text-align: center; padding: 20px;">
+		© 2018 Zic Zac Group . Được thiết kế bời ZicZac. All rights reserved.
+	</div>
+</footer>
+</body>
+</html>
