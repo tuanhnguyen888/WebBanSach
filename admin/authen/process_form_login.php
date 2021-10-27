3409a30e1f5d2929faffbf09449182bf8058a86a
TRẦN VĂN ĐIỆP <tranvandiep.it@gmail.com>
2021-10-27 18:28:54 +0700

    fix login

--- admin/authen/process_form_login.php
+++ admin/authen/process_form_login.php
@@ -6,7 +6,7 @@ if(!empty($_POST)) {
 	$pwd = getPost('password');
 	$pwd = getSecurityMD5($pwd);
 
-	$sql = "select * from User where email = '$email' and password = '$pwd' and deleted = 1";
+	$sql = "select * from User where email = '$email' and password = '$pwd' and deleted = 0";
 	$userExist = executeResult($sql, true);
 	if($userExist == null) {
 		$msg = 'Đăng nhập không thanh công, vui long kiểm tra email hoặc mật khẩu!!!';
